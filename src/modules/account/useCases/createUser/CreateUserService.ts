6dc94f67127fad29723da4e6e971c6bc963cfd57
josersn <nettorammos@hotmail.com>
2021-08-15 20:40:31 -0300

    add hash to password

--- src/modules/account/useCases/createUser/CreateUserService.ts
+++ src/modules/account/useCases/createUser/CreateUserService.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { hash } from "bcrypt";
 
 @injectable()
 class CreateUserService {
@@ -11,8 +12,15 @@ class CreateUserService {
     ) { }
 
     async execute({ name, username, password, email, driver_license }: ICreateUserDTO): Promise<void> {
+
+        const passwordHash = await hash(password, 8);
+
         await this.repository.create({
-            name, username, password, email, driver_license
+            name,
+            username,
+            password: passwordHash,
+            email,
+            driver_license
         })
     }
 
