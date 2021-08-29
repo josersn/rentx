8ef668087d86ea8ebdfc00501f1f8385b822e714
josersn <nettorammos@hotmail.com>
2021-08-29 10:29:17 -0300

    test: add supertest

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,36 @@
+import "reflect-metadata";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import swagger from "swagger-ui-express";
+
+import createConnetrion from "../typeorm";
+import "../../container";
+
+import swaggerFile from "../../../swagger.json"
+import { router } from "./routes";
+import { AppError } from "../../errors/AppError";
+
+createConnetrion();
+const app = express();
+
+app.use(express.json());
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerFile))
+app.use(router);
+
+app.use(
+    (err: Error, req: Request, res: Response, next: NextFunction) => {
+        if (err instanceof AppError) {
+            return res.status(err.statusCode).json({
+                message: err.message
+            })
+        }
+
+        return res.status(500).json({
+            status: "error",
+            message: `Internal Server Error - ${err.message}`
+        })
+    }
+)
+
+
+export { app };
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,36 +1,3 @@
-import "reflect-metadata";
-import express, { NextFunction, Request, Response } from "express";
-import "express-async-errors";
-import swagger from "swagger-ui-express";
-
-import createConnetrion from "../typeorm";
-import "../../container";
-
-import swaggerFile from "../../../swagger.json"
-import { router } from "./routes";
-import { AppError } from "../../errors/AppError";
-
-createConnetrion();
-const app = express();
-
-app.use(express.json());
-app.use("/api-docs", swagger.serve, swagger.setup(swaggerFile))
-app.use(router);
-
-app.use(
-    (err: Error, req: Request, res: Response, next: NextFunction) => {
-        if (err instanceof AppError) {
-            return res.status(err.statusCode).json({
-                message: err.message
-            })
-        }
-
-        return res.status(500).json({
-            status: "error",
-            message: `Internal Server Error - ${err.message}`
-        })
-    }
-)
-
+import { app } from "./app";
 
 app.listen("8080", () => console.log("Server is running!"));
