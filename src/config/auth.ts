2425553674a4d9056163bbd1fe652e3a2e9c4192
josersn <nettorammos@hotmail.com>
2021-09-04 10:06:41 -0300

    chore: user_token entities and config

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "990eb5a8c4047f66fc26b0fc6a60819f",
+    expires_in_token: "15m",
+    secret_refresh_token: "662eaa47199461d01a623884080934ab",
+    expires_in_refresh_token: "7d",
+    expires_refresh_token_days: 30
+}
